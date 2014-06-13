60aa48995d53b162a1b2c86d73ea24c9ad7b3041
Brian Di Palma <offler@gmail.com>
2014-06-13 21:17:01 +0100

    Correct spacing.

--- src/ContactsListConstants.js
+++ src/ContactsListConstants.js
@@ -1,5 +1,5 @@
 export var ContactsListConstants = {
 	CONTACT_SELECTED: {},
-    CONTACT_STATUS_CHANGED: {},
-    CONTACTS_LIST_RECEIVED: {}
+	CONTACT_STATUS_CHANGED: {},
+	CONTACTS_LIST_RECEIVED: {}
 }
