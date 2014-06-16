9cacc7b3c96759e4dcbbdd46858a5e7b9856f5c4
Brian Di Palma <offler@gmail.com>
2014-06-16 22:26:19 +0100

    Return the contacts map from getState.
    Also add a bunch of default contacts to populate the contacts list.

--- src/ContactsListStore.js
+++ src/ContactsListStore.js
@@ -2,10 +2,15 @@ import {Store} from 'flux-es6';
 
 import {ContactsListConstants} from './ContactsListConstants';
 
-var contacts = new Map();
+var contacts = new Map([
+	["guybedford", {"status": "online", "imageSource": "https://avatars0.githubusercontent.com/u/598730?s=140"}],
+	["arv", {"status": "online", "imageSource": "https://avatars3.githubusercontent.com/u/45845?s=140"}],
+	["kybernetikos", {"status": "offline", "imageSource": "https://avatars3.githubusercontent.com/u/430980?s=140"}]
+]);
 
 export default class extends Store {
 	getState() {
+		return contacts;
 	}
 
 	handleAction(payload) {
